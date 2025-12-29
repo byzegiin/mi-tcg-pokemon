b2b5aa569f0f1aea6ba2d7e07e9157836873f885
byzegiin <rapadicto_raul90@hotmail.com>
2025-12-29 16:01:29 -0600

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir el frontend
+// Servir frontend
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(__dirname));
