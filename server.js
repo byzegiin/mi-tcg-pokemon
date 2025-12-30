5fe8b90c13bfbe925bff3331322e51d3158689af
byzegiin <rapadicto_raul90@hotmail.com>
2025-12-29 21:38:52 -0600

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Servir archivos de la carpeta public
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+// Si el navegador pide algo que no existe, enviamos el index.html para evitar el 404
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
+app.listen(PORT, () => console.log(`Servidor activo en puerto ${PORT}`));
